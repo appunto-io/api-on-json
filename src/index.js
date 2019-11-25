49e3e4d5d53be67a7ce0ffec09daedb40af7be7c
Anas Jouhdy <anas.jouhdy@gmail.com>
2019-11-25 15:59:55 +0100

    Version 0.0.3 - Adding Mongo and Rethink class in the npm package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const { DataModel } = require('./datamodel/datamodel.js');
 const { ApiModel }  = require('./apimodel/apimodel.js');
 const { Server }    = require('./server/server.js');
+const { Rethink,
+        Mongo }     = require('./databases/databases.js')
 
 
 module.exports = {
   DataModel,
   ApiModel,
-  Server
+  Server,
+  Rethink,
+  Mongo,
 };
